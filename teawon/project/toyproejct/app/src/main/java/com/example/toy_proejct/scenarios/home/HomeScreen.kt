43892c34f761c708cf93fb0ce0d7871047242048
teawon <xodnjsvkvk9@gmail.com>
2022-11-05 19:49:40 +0900

    feat : SearchBar Component

--- teawon/project/toyproejct/app/src/main/java/com/example/toy_proejct/scenarios/home/HomeScreen.kt
+++ teawon/project/toyproejct/app/src/main/java/com/example/toy_proejct/scenarios/home/HomeScreen.kt
@@ -1,8 +1,208 @@
 package com.example.toy_proejct.scenarios.home
 
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel) {
     //화면에 보여지는 부분을 구성
+    Search(viewModel)
 }
+
+@Composable
+private fun Search(viewModel: HomeViewModel) {
+    val searchWidgetState by viewModel.searchWidgetState //활성화 여부
+    val searchTextState by viewModel.searchTextState // 검색 변수
+
+    Scaffold(
+        topBar = {
+            SearchBar(
+                searchWidgetState = searchWidgetState,
+                searchTextState = searchTextState,
+                onTextChange = {
+                    viewModel.updateSearchTextState(newValue = it) //텍스트 값이 바뀌면 해당 텍스트로 변수 업데이트
+                },
+                onCloseClicked = {
+                    viewModel.updateSearchWidgetState(newState = false) // 버튼이 눌리면 Search영역 비활성화
+
+                },
+                onSearchClicked = {
+                    Log.d("Searched Text", it) //검색버튼이 눌리면 특정 함수 실행
+                },
+                onSearchTriggered = {
+                    viewModel.updateSearchWidgetState(newState = true) //Search영역이 클릭되면 Search영역 활성화
+                    viewModel.updateSearchTextState("")
+                }
+            )
+        }
+    ) {}
+}
+
+
+@Composable
+fun SearchBar(
+    searchWidgetState: Boolean,
+    searchTextState: String,
+    onTextChange: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit,
+    onSearchTriggered: () -> Unit
+) {
+    when (searchWidgetState) {
+       false -> {
+            DefaultAppBar(
+                onSearchClicked = onSearchTriggered, //영역이 비활성화라면 초기에 보여줄 컴포넌트로 보여주기
+                text = searchTextState
+            )
+        }
+        true -> {
+            SearchAppBar(
+                text = searchTextState,
+                onTextChange = onTextChange,
+                onCloseClicked = onCloseClicked,
+                onSearchClicked = onSearchClicked
+            )
+        }
+    }
+}
+
+@Composable
+fun DefaultAppBar(onSearchClicked: () -> Unit , text: String) {
+    TopAppBar(
+        title = {
+            if(text.isNotEmpty()){
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = text,
+                    textAlign = TextAlign.Center
+                )
+            }
+            else{
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text="상품 검색",
+                    textAlign = TextAlign.Center
+
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                onClick = { onSearchClicked() } //버튼 클릭시 Search영역 활성화
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "Search Icon",
+                    tint = Color.White
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun SearchAppBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit,
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        elevation = AppBarDefaults.TopAppBarElevation,
+        color = MaterialTheme.colors.primary
+    ) {
+        TextField(modifier = Modifier
+            .fillMaxWidth(),
+            value = text,
+            onValueChange = {
+                onTextChange(it)
+            },
+            placeholder = {
+                Text(
+                    modifier = Modifier
+                        .alpha(ContentAlpha.medium),
+                    text = "검색하기...",
+                    color = Color.White
+                )
+            },
+            textStyle = TextStyle(
+                fontSize = MaterialTheme.typography.subtitle1.fontSize
+            ),
+            singleLine = true,
+            leadingIcon = {
+                IconButton(
+                    modifier = Modifier
+                        .alpha(ContentAlpha.medium),
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search Icon",
+                        tint = Color.White
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                            onCloseClicked() //취소 버튼 누르면 closeClick()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Close Icon",
+                        tint = Color.White
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearchClicked(text)
+                    onCloseClicked()
+                }
+            ),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent,
+                cursorColor = Color.White.copy(alpha = ContentAlpha.medium)
+            ))
+    }
+}
+
+
+//@Composable
+//@Preview
+//fun DefaultAppBarPreview() {
+//    DefaultAppBar(onSearchClicked = {})
+//}
+//
+//@Composable
+//@Preview
+//fun SearchAppBarPreview() {
+//    SearchAppBar(
+//        text = "Some random text",
+//        onTextChange = {},
+//        onCloseClicked = {},
+//        onSearchClicked = {}
+//    )
+//}
--- teawon/project/toyproejct/app/src/main/java/com/example/toy_proejct/scenarios/home/HomeViewModel.kt
+++ teawon/project/toyproejct/app/src/main/java/com/example/toy_proejct/scenarios/home/HomeViewModel.kt
@@ -1,7 +1,26 @@
 package com.example.toy_proejct.scenarios.home
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
+
+    private val _searchWidgetState: MutableState<Boolean> = //검색창 활성화 여부
+        mutableStateOf(value = false)
+    val searchWidgetState: State<Boolean> = _searchWidgetState
+
+    private val _searchTextState: MutableState<String> = //검색 텍스트 값
+        mutableStateOf(value = "")
+    val searchTextState: State<String> = _searchTextState
+
+    fun updateSearchWidgetState(newState: Boolean) { //활성화 여부 변경 함수
+        _searchWidgetState.value = newState
+    }
+
+    fun updateSearchTextState(newValue: String) { //검색 텍스트 값 변경 함수
+        _searchTextState.value = newValue
+    }
     //API구현
 }
