4b778dadbf53f834f24ee3d467f79f3ae6be8eb5
teawon <xodnjsvkvk9@gmail.com>
2022-11-10 02:09:55 +0900

    feat : DetailScreen Design

--- teawon/project/toyproejct/app/src/main/java/com/example/toy_proejct/scenarios/detail/DetailScreen.kt
+++ teawon/project/toyproejct/app/src/main/java/com/example/toy_proejct/scenarios/detail/DetailScreen.kt
@@ -1,11 +1,13 @@
 package com.example.toy_proejct.scenarios.detail
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 
-import android.content.Intent
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -23,11 +25,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import coil.compose.AsyncImage
 import com.example.toy_proejct.api.getDetail.DetailDto
 import com.example.toy_proejct.api.getDetail.MallDtoInfo
@@ -70,17 +74,18 @@ private fun DetailComponent(
                     back()
                 })
             Text(
-                text = itemList.title, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center
+                text = itemList.title, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center , style = MaterialTheme.typography.h5
             )
         }
-        Text(
-            text = itemList.minimumPrice.toString(), modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center
-        )
-        AsyncImage(
-            model = itemList.image, //
-            contentDescription = "image"
-        )
 
+            AsyncImage(
+                model = itemList.image, //
+                contentDescription = "image",
+                modifier = Modifier.fillMaxWidth()
+            )
+        Text(
+            text = "최저가 : ${itemList.minimumPrice.toString()}", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center
+        )
 
         LazyColumn{
             items(items = itemList.mallDtoInfo){
@@ -94,35 +99,45 @@ private fun DetailComponent(
 fun MallList(item: MallDtoInfo) { //각 상품에 대한 설명
     val context = LocalContext.current
 
+
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(120.dp)
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Column(modifier = Modifier.padding(4.dp)) {
-                Text(
-                    text = item.link,
-                    style = MaterialTheme.typography.h6
-                )
-                Text(
-                    text = item.link,
-                    style = MaterialTheme.typography.h6
-                )
-                Text(
-                    text = item.name,
-                    style = MaterialTheme.typography.h6
-                )
-                Text(
-                    text = item.paymentOption,
-                    style = MaterialTheme.typography.h6
+            .height(60.dp)
+            .clickable {
+                context.startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse(item.link)
+                    )
                 )
             }
+    ) {
 
-        }
+            Column(modifier = Modifier.padding(2.dp)) {
+                Row( modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                    Text(
+                        text = item.name,
+                        style = MaterialTheme.typography.h6
+                    )
+                    Text(
+                        text = " ${item.price.toString()}원",
+                        style = MaterialTheme.typography.h6,
+                        color = Color.Blue
+
+                    )
+                }
+//                Text(
+//                    text = item.name,
+//                    style = MaterialTheme.typography.h6
+//                )
+//                Text(
+//                    text = item.paymentOption,
+//                    style = MaterialTheme.typography.h6
+//                )
+            }
+
+
     }
 }
